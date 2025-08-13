5630d0e045c8992c706ee421bd1633ae80e18109
FertilCenter <sistemas@fertilcenter.com.mx>
2025-08-13 09:42:30 -0600

    Correccion de rutas para next

--- next.config.js
+++ next.config.js
@@ -2,24 +2,18 @@
 const isDev = process.env.NODE_ENV === 'development'
 
 const nextConfig = {
-  // RESTAURAR basePath porque Apache envía /webpage/
   basePath: '/webpage',
   assetPrefix: '/webpage',
   trailingSlash: true,
   
-  // Configuración de salida para Docker
   output: 'standalone',
   
   images: {
+    // SOLUCIÓN: Deshabilitar optimización
+    unoptimized: true,
     domains: ['www.fertilcenter.com.mx', 'fertilcenter.com.mx'],
-    path: '/webpage/_next/image',
-    unoptimized: false,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    formats: ['image/webp'],
-    minimumCacheTTL: 60,
-    dangerouslyAllowSVG: false,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   
   experimental: {
